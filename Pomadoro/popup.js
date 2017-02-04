43287791bb496c39f60e8fca379531f665348eee
Jason Chrin <jasonchrin@gmail.com>
2017-02-04 18:43:22 -0500

    Add in breaks Allow for timer to be stopped Background and text update

--- Pomadoro/popup.js
+++ Pomadoro/popup.js
@@ -1,45 +1,82 @@
 var timerType = 0;
 /* 0 = pomadoro; 1 = short break; 2=longbreak; */
-var breakCounter = 0;
+var breakCounter = 1;
 var pomadoroTime = 1500;
 var shortBreakTime = 300;
 var longBreakTime = 900;
+var longBreakNumber = 3;
 var timerState = 0; // 0= stopped; 1= running;
 var duration = 0;
+var currentTime = pomadoroTime;
+var display;
+var timerbutton;
+var runningTimer;
 
 document.addEventListener('DOMContentLoaded', function() {
-  var timerbutton = document.getElementById('startTimer');
-  var display = document.getElementById('timer');
+    timerbutton = document.getElementById('startTimer');
+    currentTime = pomadoroTime;
+    duration = pomadoroTime;
+    display = document.getElementById('timer');
+    display.innerHTML = timeToString();
   timerbutton.addEventListener('click', function() {
-		buttonClicked(display);
+		buttonClicked();
   }, false);
 }, false);
 
-function buttonClicked(display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        
+function buttonClicked() {
+    currentTime = duration;
+    if(timerState == 0){
+        runningTimer = setInterval(calculateTimer, 1000);
+        timerbutton.innerHTML = "Stop Timer";
+        timerState = 1;
+    }
+    else{
+        clearInterval(runningTimer);
+        getNextTimer();
+        timerbutton.innerHTML = "Start Timer";
+        timerState = 0;
         display.innerHTML = timeToString();
 
-        if (--timer < 0) {
-            timer = duration;
-			
-        }
-    }, 1000);
+    }
 }
 
-window.onload = function () {
-        var duration = pomadoroTime;
-        display = document.getElementById('timer');
-   	display.innertHTML = timeToString();
-};
+
+function getNextTimer() {
+    if (timerType == 0) {
+
+        if (breakCounter % longBreakNumber == 0) {
+            timerType = 2;
+            duration = longBreakTime;
+
+        }
+        else {
+            timerType = 1;
+            duration = shortBreakTime;
+        }
+        document.querySelector("body").className = "break";
+        breakCounter++;
+    }
+    else {
+        document.querySelector("body").className = "pomadoro";
+        timerType = 0;
+        duration = pomadoroTime;
+    }
+    currentTime = duration;
+}
+
+function calculateTimer(){
+    if (--currentTime < 0) {
+        getNextTimer();
+    }
+    display.innerHTML = timeToString();
+}
+
 
 function timeToString(){
     var minutes, seconds;
-	minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+	minutes = parseInt(currentTime / 60, 10);
+    seconds = parseInt(currentTime % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 	return minutes + ":" + seconds;
 }
