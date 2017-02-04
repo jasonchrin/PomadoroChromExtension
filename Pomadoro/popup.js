e40170ccd06cd364a255c96343473510bce4d19c
Jason Chrin <jasonchrin@gmail.com>
2017-02-04 15:01:28 -0500

    temp checkin

--- Pomadoro/popup.js
+++ Pomadoro/popup.js
@@ -1,21 +1,25 @@
+var timerType = 0;
+/* 0 = pomadoro; 1 = short break; 2=longbreak; */
+var breakCounter = 0;
+var pomadoroTime = 1500;
+var shortBreakTime = 300;
+var longBreakTime = 900;
+var timerState = 0; // 0= stopped; 1= running;
+var duration = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
   var timerbutton = document.getElementById('startTimer');
   var display = document.getElementById('timer');
   timerbutton.addEventListener('click', function() {
-		startTimer(1500, display);
+		buttonClicked(display);
   }, false);
 }, false);
 
-function startTimer(duration, display) {
+function buttonClicked(display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.innerHTML = minutes + ":" + seconds;
+        
+        display.innerHTML = timeToString();
 
         if (--timer < 0) {
             timer = duration;
@@ -25,7 +29,17 @@ function startTimer(duration, display) {
 }
 
 window.onload = function () {
-
+        var duration = pomadoroTime;
         display = document.getElementById('timer');
-   
+   	display.innertHTML = timeToString();
 };
+
+function timeToString(){
+    var minutes, seconds;
+	minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+	return minutes + ":" + seconds;
+}
